df606bd0a7e19dda62cde8cbdf79dc4da1ee3e1d
Ndindairene <irene.ndindaa@gmail.com>
2023-11-03 14:18:22 +0300

    html and css changes

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+
+//api key
+const APIKEY = "AIzaSyBh8dlHW3oIhYshT0qT3ePRcN2eb6wzgxc";
+
+//adding an event listener to the search button
+
+document.getElementById("searchButton").addEventListener("click", () => {
+  const searchInput = document.getElementById("searchInput").value;
+  if (searchInput) {
+    searchBooks(searchInput);
+  }
+});
+})
